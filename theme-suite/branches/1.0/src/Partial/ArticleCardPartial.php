35cc6ab867ecf600e47c833d1b63eb64e734a36a
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2020-12-26 13:36:14 +0000

    theme-suite v1.0.x-dev

--- theme-suite/branches/1.0/src/Partial/ArticleCardPartial.php
+++ theme-suite/branches/1.0/src/Partial/ArticleCardPartial.php
@@ -2,7 +2,7 @@
 
 namespace Pollen\ThemeSuite\Partial;
 
-use Pollen\ThemeSuite\Contracts\QueryPostComposing;
+use Pollen\ThemeSuite\Query\QueryPostComposingInterface;
 use tiFy\Wordpress\Contracts\Query\QueryPost as QueryPostContract;
 use tiFy\Wordpress\Query\QueryPost as post;
 
@@ -31,7 +31,7 @@ class ArticleCardPartial extends AbstractPartialDriver
     public function render(): string
     {
         if ($article = ($p = $this->get('post', null)) instanceof QueryPostContract ? $p : post::create($p)) {
-            if ($article instanceof QueryPostComposing) {
+            if ($article instanceof QueryPostComposingInterface) {
                 $enabled = array_merge($article->getArchiveComposing('enabled', []), $this->get('enabled', []));
 
                 $this->set('thumbnail', $article->getBanner());
